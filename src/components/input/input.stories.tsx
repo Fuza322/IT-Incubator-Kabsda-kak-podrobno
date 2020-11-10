fe515092ba991772857fbd30eb9fc9b38d490a50
Fuza322 <vanya.osipik322@gmail.com>
2020-11-10 05:57:05 +0300

    13

--- src/components/input/input.stories.tsx
+++ src/components/input/input.stories.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, useRef, useState} from 'react';
+import {action} from '@storybook/addon-actions';
 import {OnOff} from '../OnOff/OnOff';
 
 export default {
@@ -8,7 +9,7 @@ export default {
 
 export const UncontrolledInput = () => {
     return (
-        <input/>
+        <input />
     )
 }
 
@@ -20,7 +21,7 @@ export const TrackValueOfUncontrolledInput = () => {
     }
 
     return (
-        <> <input value={value} onChange={ onChangeClick }/> - {value} </>
+        <> <input onChange={ onChangeClick }/> - {value} </>
     )
 }
 
@@ -34,12 +35,52 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     }
 
     return (
-        <> <input ref={inputRef} /> <button onClick={ saveClick }>Save</button> - actual value: {value} </>
+        <> <input ref={inputRef}/>
+            <button onClick={saveClick}>Save</button>
+            - actual value: {value} </>
     )
 }
 
 export const ControlledInputWithFixedValue = () => {
     return (
         <input value='IT-Incubator.by'/>
+    )
+}
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+    const onChangeClick = (event: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(event.currentTarget.value)
+    }
+
+    return (
+        <input value={parentValue} onChange={ onChangeClick }/>
+    )
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true)
+    const onChangeClick = (event: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(event.currentTarget.checked)
+    }
+
+    return (
+        <input type='checkbox' checked={parentValue} onChange={ onChangeClick }/>
+    )
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('0')
+    const onChangeClick = (event: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(event.currentTarget.value)
+    }
+
+    return (
+        <select value={parentValue} onChange={onChangeClick}>
+            <option value={'0'}>none</option>
+            <option value={'1'}>Minsk</option>
+            <option value={'2'}>Moscow</option>
+            <option value={'3'}>Kiev</option>
+        </select>
     )
 }
