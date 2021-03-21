f4166dcac60b92b6fbc31741b21bca6438204912
Fuza322 <vanya.osipik322@gmail.com>
2021-03-21 16:50:01 +0300

    lesson 21 useEffect

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -11,17 +11,17 @@ export const SimpleExample = () => {
     const [counter, setCounter] = useState(1)
     const [fake, setFake] = useState(1)
 
-    useEffect( ()=> {
+    useEffect(() => {
         console.log('UseEffect every component render')
         document.title = counter.toString()
     })
 
-    useEffect( ()=> {
+    useEffect(() => {
         console.log('UseEffect only first component render')
         document.title = counter.toString()
     }, [])
 
-    useEffect( ()=> {
+    useEffect(() => {
         console.log('UseEffect first component render and counter changed')
         document.title = counter.toString()
     }, [counter])
@@ -29,8 +29,44 @@ export const SimpleExample = () => {
     return (
         <>
             Hello, {counter} {fake}
-            <button onClick={() => {setFake(fake + 1)}}>Fake+</button>
-            <button onClick={() => {setCounter(counter + 1)}}>Counter+</button>
+            <button onClick={() => {
+                setFake(fake + 1)
+            }}>Fake+
+            </button>
+            <button onClick={() => {
+                setCounter(counter + 1)
+            }}>Counter+
+            </button>
+        </>
+    )
+}
+
+export const SetTimeOutExample = () => {
+
+    console.log('SetTimeOutExample')
+
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+
+    useEffect(() => {
+
+        setInterval(() => {
+            console.log("tick: " + counter)
+            setCounter(state => state + 1)
+            }, 1000)
+    }, [])
+
+    return (
+        <>
+            Hello, counter: {counter} - fake: {fake}
+            {/*<button onClick={() => {*/}
+            {/*    setFake(fake + 1)*/}
+            {/*}}>Fake+*/}
+            {/*</button>*/}
+            {/*<button onClick={() => {*/}
+            {/*    setCounter(counter + 1)*/}
+            {/*}}>Counter+*/}
+            {/*</button>*/}
         </>
     )
 }
